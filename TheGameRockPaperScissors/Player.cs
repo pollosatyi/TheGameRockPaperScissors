fedbf5a1a0ff813df3bfa8e7f73f02027f4ed717
pollosatyi <88507116+pollosatyi@users.noreply.github.com>
2024-11-29 12:32:12 +0300

    Add project files.

---
+++ TheGameRockPaperScissors/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGameRockPaperScissors
+{
+    public abstract class Player
+    {
+        public string Name {  get; set; }
+
+
+
+        public 
+
+        //public Player(string name)
+        //{
+        //    Name = name;
+        //}
+    }
+}
---
+++ TheGameRockPaperScissors/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TheGameRockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Игра Камень, Ножницы, Бумага");
+            bool isGameWork=true;
+            while (isGameWork)
+            {
+                Console.WriteLine("0 для выхода");
+                Console.WriteLine("1 для игры человек против компьютера");
+                Console.WriteLine("2 для игры человек против человека");
+                Console.WriteLine("3 для игры компьютер против компьютера");
+                Console.WriteLine("Введите число: ");
+                if(int.TryParse(Console.ReadLine(),out int gameNumber))
+                {
+                    switch(gameNumber)
+                    {
+                        case 0:isGameWork = false; break;
+                        case 1:  break;
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Это не число.Введите заново");
+                }
+            }
+        }
+    }
+}
